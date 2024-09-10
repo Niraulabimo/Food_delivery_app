4003e00eeb2fa8cdc45fb9caae619ace52e45505
Your Name <bimochanniraula08@gmail.com>
2024-09-10 22:51:44 +0545

    complete

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import { assets } from "../../assets/assets";
+
 const Orders = ({ url }) => {
   const [orders, setOrders] = useState([]);
 
@@ -17,6 +18,17 @@ const Orders = ({ url }) => {
       toast.error("Error");
     }
   };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
@@ -58,7 +70,10 @@ const Orders = ({ url }) => {
             </div>
             <p>Items: {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+            >
               <option value="Food processing">Food processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
